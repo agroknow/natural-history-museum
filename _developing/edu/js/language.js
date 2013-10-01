5788ba9a1bb5ff8ed6f61d3715eb59eace5ae1ec
Theodore Mathioudakis <tmathioudakis@gmail.com>
2013-10-01 12:05:50 +0300

    ui changes and bug fixes

--- _developing/edu/js/language.js
+++ _developing/edu/js/language.js
@@ -57,7 +57,7 @@ function language()
 	if(lang!=undefined)
 	{
 		jQuery.ajax({
-		url: "http://greenlearningnetwork.org/finders_files/nhmc_translations/"+lang_file.replace("#", "")+".json",
+		url: "http://83.212.100.142/finders_files/nhmc_translations/"+lang_file.replace("#", "")+".json",
 		dataType: 'json',
 		/* jsonp: 'callback', */
 		/* jsonpCallback: 'jsonCallback', */
